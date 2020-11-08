125bfe03d7b5f5f38af3a4b1bfdbc46b2192f6b4
James Dai <K038246@eltham-college.org.uk>
2020-11-08 17:42:34 +0000

    08/11/2020 work
    Known problem: Function unable to access array of objects

---
+++ Bank Account.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_Account
+{
+    class Bank_Account
+    {
+        // Create values
+        private string FullName, Address;
+        private int BalancePennies, AccountNumber;
+
+
+        // Set up
+        public Bank_Account(string name, string address, int num, int pennies)
+        {
+            FullName = name;
+            FullName = address;
+            AccountNumber = num;
+            BalancePennies = pennies;
+        }
+
+
+        public Bank_Account()
+        {
+            FullName = "";
+            FullName = "";
+            AccountNumber = 0;
+            BalancePennies = 0;
+        }
+
+        
+        // Get/set balance
+        private void setBalance(int n)
+        {
+            BalancePennies = n;
+        }
+        public int getBalance()
+        {
+            return BalancePennies;
+        }
+
+
+        // print balance
+        public void displayBalance()
+        {
+            Console.WriteLine("Your balance is: £" + Convert.ToString(getBalance() / 100));
+        }
+
+
+        // Make withdrawal and deposit
+        public void withdraw(int monies)
+        {
+            setBalance(getBalance()-monies);
+            Console.WriteLine("You have withdrawn: £" + Convert.ToString(monies / 100));
+            displayBalance();
+        }
+
+        public void deposit(int monies)
+        {
+            setBalance(getBalance() + monies);
+            Console.WriteLine("You have withdrawn: £" + Convert.ToString(monies / 100));
+            displayBalance();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Bank_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool ContinueRunning = true;
+            //Create references
+            Bank_Account[] BA = new Bank_Account[2];
+
+            //Create objects
+            BA[0] = new Bank_Account("Adam", "Somewhere", 0, 0);
+            BA[1] = new Bank_Account("Bill", "Nowhere", 1, 6000);
+            BA[2] = new Bank_Account("Chip", "Here", 2, 200000);
+
+            while(true)
+            {
+                Console.WriteLine("PLease input acount number to access: ");
+                int Current_user = Convert.ToInt32(Console.ReadLine()); ;
+                do
+                {
+                    ContinueRunning = MainMenu(Current_user);
+                }
+                while (ContinueRunning == true);
+            }
+        }
+
+        static bool MainMenu(int AccountNumber)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) View Balance");
+            Console.WriteLine("2) Make a deposit");
+            Console.WriteLine("3) Make a withdrawal");
+            Console.WriteLine("Anything else to exit");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    BA[AccountNumber].displayBalance();
+                    return true;
+                case "2":
+                    BA[AccountNumber].displayBalance();
+                    Console.WriteLine("How much would you like to depoit(in pennies)?");
+                    Console.WriteLine("Enter zero (0) to cancel.");
+                    BA[AccountNumber].deposit(Console.ReadLine);
+                    return true;
+                case "3":
+                    BA[AccountNumber].displayBalance();
+                    Console.WriteLine("How much would you like to withdraw(in pennies)?");
+                    Console.WriteLine("Enter zero (0) to cancel.");
+                    BA[AccountNumber].withdraw(Console.ReadLine);
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
